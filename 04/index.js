49e43576d5e087e066169019f5cee1b2edeb8222
jull128 <yul.perelygina@yandex.ru>
2023-09-21 12:44:40 +0500

    upd

--- 04/index.js
+++ 04/index.js
@@ -2,13 +2,13 @@ let declOfNum = (n) => {
   n = Math.abs(amount) % 100;
   let n1 = n % 10;
   if (n > 10 && n < 20) {
-    return "товаров";
+    return "сообщений";
   }
   if (n1 > 1 && n1 < 5) {
-    return "товара";
+    return "сообщения";
   }
   if (n1 == 1) {
-    return "товар";
+    return "сообщение";
   }
-  return "товаров";
+  return "сообщений";
 };
